2546f4eadef9bde8d3e881a0f91c275217151664
hector <hectorlopezp1998@gmail.com>
2022-01-14 16:11:25 -0500

    fetch pokemon data

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ const colors = {
   fighting: "#E6E0D4",
   normal: "#F5F5F5",
 }
+
+const fecthPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i)
+  }
+}
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data);
+}
+
+fecthPokemons()
